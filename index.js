7d5f0a713ff41e092e0c1dda6d45495adfd97296
Kai <118097717+KaisoulTV@users.noreply.github.com>
2025-07-08 20:20:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const keep_alive = require('./keep_alive.js')
 // Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.token);
 
-bot.on("ready", () => {
-  console.log("I'm ready, changing status")
-  bot.editStatus("dnd", {
-    name: "rbxrise.com",
-    type: 0
-  });
-})
+bot.on("ready", async () => { // When the bot is ready
+  console.log("Ready!");
+  bot.editStatus("dnd", [
+    state: "rbxrise.com",
+    type: 4
+  ]);
+});
 
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
